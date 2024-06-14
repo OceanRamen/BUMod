e08dba096da67578898c7850d7a3e21b2e5cdfff
OceanRamen <wt557978@gmail.com>
2024-06-14 20:31:34 +0100

    feat(Custom Sprite): Added Custom DrSpectred sprite and replaced main menu card with it.

--- mod.lua
+++ mod.lua
@@ -9,40 +9,24 @@ end
 function initAssets()
 	local lovely = require("lovely")
 	local nativefs = require("nativefs")
-	assert(load(nativefs.read(lovely.mod_dir .. "/BUMod/assets/2x/test.lua")))() -- test.lua: print("HELLO FROM TEST.LUA")
-	--  OUTPUT: "HELLO FROM TEST.LUA"
-	print(nativefs.getDirectoryItems(lovely.mod_dir .. "/BUMod/assets/2x/"))
-	local assets = {
-		{
-			name = "Joker",
-			path = lovely.mod_dir .. "/BUMod/assets/" .. G.SETTINGS.GRAPHICS.texture_scaling .. "x/Jokers.png",
-			px = 71,
-			py = 95,
-		},
+	local asset = {
+		name = "Joker",
+		path = lovely.mod_dir .. "/BUMod/assets/" .. G.SETTINGS.GRAPHICS.texture_scaling .. "x/Jokers.png",
+		px = 71,
+		py = 95,
 	}
-	G.ASSET_ATLAS = G.ASSET_ATLAS or {}
+	
+	local file_data =
+		assert(nativefs.newFileData(asset.path), ("Failed to collect file data for Atlas %s"):format(asset.name))
+	local image_data =
+		assert(love.image.newImageData(file_data), ("Failed to initialize image data for Atlas %s"):format(asset.name))
+	local image = love.graphics.newImage(image_data, { mipmaps = true, dpiscale = G.SETTINGS.GRAPHICS.texture_scaling })
 
-	for i = 1, #assets do
-		local asset = assets[i]
-		local path = asset.path
-		print("Loading asset from path:", path)
-
-		if not nativefs.getInfo(path) then
-			print("Error: Path does not exist:", path)
-		end
-
-		local success, image =
-			pcall(love.graphics.newImage, path, { mipmaps = true, dpiscale = G.SETTINGS.GRAPHICS.texture_scaling })
-		if success then
-			G.ASSET_ATLAS[asset.name] = {
-				name = asset.name,
-				image = image,
-				type = asset.type,
-				px = asset.px,
-				py = asset.py,
-			}
-		else
-			print("Error loading image:", image)
-		end
-	end
+	G.ASSET_ATLAS[asset.name] = {
+		name = asset.name,
+		image = image,
+		type = asset.type,
+		px = asset.px,
+		py = asset.py,
+	}
 end
