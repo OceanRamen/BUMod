6e9907b6de7b755b870485aad5dbfb8902807cbc
makatymba2001 <makatymba2001@yandex.ru>
2025-04-18 15:32:03 +0300

    Balatro x Balatro collab

--- main.lua
+++ main.lua
@@ -63,10 +63,112 @@ function BUMod.table_merge(target, source, ...)
 	return target
 end
 
+---------
+
+-- SMODS format
+BUMod.COLLABS = {
+	{
+		key = "balatro_clubs",
+		suit = "Clubs",
+		loc_txt = "Balatro",
+	},
+	{
+		key = "balatro_diamonds",
+		suit = "Diamonds",
+		loc_txt = "Balatro",
+	},
+	{
+		key = "balatro_hearts",
+		suit = "Hearts",
+		loc_txt = "Balatro",
+	},
+	{
+		key = "balatro_spades",
+		suit = "Spades",
+		loc_txt = "Balatro",
+	},
+}
+
+-- Malverk format
+BUMod.CARDS = {
+	{
+		key = "joker_scholar_drspectred",
+		path = "single_jokers/j_scholar.png",
+		set = "Joker",
+		keys = { "j_scholar" },
+		loc_txt = {
+			name = "Scholar DrSpectred",
+			text = { "Replace Scholar with DrSpectred" },
+		},
+	},
+	{
+		key = "joker_showman_chip",
+		path = "single_jokers/j_ring_master.png",
+		set = "Joker",
+		keys = { "j_ring_master" },
+		loc_txt = {
+			name = "Showman Chip",
+			text = { "Replace Showman with Chip" },
+		},
+	},
+	{
+		key = "joker_space_zaino",
+		path = "single_jokers/j_space.png",
+		set = "Joker",
+		keys = { "j_space" },
+		loc_txt = {
+			name = "Spaceman Zaino",
+			text = { "Replace Spaceman with Zaino" },
+		},
+	},
+	{
+		key = "joker_duo_kitty_and_perkeo",
+		path = "single_jokers/j_duo.png",
+		set = "Joker",
+		keys = { "j_duo" },
+		loc_txt = {
+			name = "Kitty and Perkeo Duo",
+			text = { "Replace The Duo with Kitty and Perkeo" },
+		},
+	},
+	{
+		key = "joker_family_wingcap",
+		path = "single_jokers/j_family.png",
+		set = "Joker",
+		keys = { "j_family" },
+		loc_txt = {
+			name = "Wingcap's Family",
+			text = { "Replace The Family with Wingcap's Family" },
+		},
+	},
+	{
+		key = "joker_idol_stupid_cat",
+		path = "single_jokers/j_idol.png",
+		set = "Joker",
+		keys = { "j_idol" },
+		loc_txt = {
+			name = "Stupid's Cat Idol",
+			text = { "Replace The Idol with Stupid's Cat Idol" },
+		},
+	},
+	{
+		key = "tarot_hung_man",
+		path = "single_tarots/c_hanged_man.png",
+		set = "Tarot",
+		keys = { "c_hanged_man" },
+		loc_txt = {
+			name = "Hung Man",
+			text = { "Replace The Hanged Man with The Hung Man" },
+		},
+	},
+}
+
+---------
+
+BUMod.language_buffer = nil
 function BUMod.get_localization()
 	return assert(loadstring(nativefs.read(BUMod.PATH .. "/loc_files/bu.lua")))()
 end
-
 function BUMod.setup_language()
 	G.LANGUAGES["bu"] = {
 		font = 1,
@@ -80,7 +182,6 @@ function BUMod.setup_language()
 		},
 	}
 end
-
 local game_set_language_ref = Game.set_language
 function Game:set_language(...)
 	-- Store initially loaded language
@@ -93,7 +194,6 @@ function Game:set_language(...)
 
 	return game_set_language_ref(self, ...)
 end
-
 local init_localization_ref = init_localization
 function init_localization(...)
 	-- If initially loaded language is BU, select it
@@ -107,48 +207,9 @@ function init_localization(...)
 	if G.SETTINGS.language == "bu" then
 		G.localization = BUMod.table_merge({}, G.localization, BUMod.get_localization())
 	end
+	BUMod.setup_collabs_localization()
+
 	return init_localization_ref(...)
-end
-
-function BUMod.setup_sprites()
-	-- Don't do anything if SMODS present
-	if SMODS and SMODS.can_load then
-		return
-	end
-
-	local assets_to_load = {
-		{
-			name = "Joker",
-			path = BUMod.PATH .. "/assets/" .. G.SETTINGS.GRAPHICS.texture_scaling .. "x/Jokers.png",
-			px = 71,
-			py = 95,
-		},
-		{
-			name = "Tarot",
-			path = BUMod.PATH .. "/assets/" .. G.SETTINGS.GRAPHICS.texture_scaling .. "x/Tarots.png",
-			px = 71,
-			py = 95,
-		},
-	}
-
-	for _, asset in ipairs(assets_to_load) do
-		local file_data =
-			assert(nativefs.newFileData(asset.path), ("Failed to collect file data for Atlas %s"):format(asset.name))
-		local image_data = assert(
-			love.image.newImageData(file_data),
-			("Failed to initialize image data for Atlas %s"):format(asset.name)
-		)
-		local image =
-			love.graphics.newImage(image_data, { mipmaps = true, dpiscale = G.SETTINGS.GRAPHICS.texture_scaling })
-
-		G.ASSET_ATLAS[asset.name] = {
-			name = asset.name,
-			image = image,
-			type = asset.type,
-			px = asset.px,
-			py = asset.py,
-		}
-	end
 end
 
 local use_and_sell_buttons_ref = G.UIDEF.use_and_sell_buttons
@@ -204,4 +265,107 @@ function G.UIDEF.use_and_sell_buttons(card)
 	return buttons
 end
 
--- G.pack_cards:emplace(create_card('Spectral', G.pack_cards, nil, nil, nil, nil, "c_immolate"))
+---------
+
+BUMod.is_collabs_injected = false
+BUMod.collabs_index_map = {
+	Clubs = {},
+	Diamonds = {},
+	Hearts = {},
+	Spades = {},
+}
+function BUMod.load_asset(asset)
+	local file_data =
+		assert(nativefs.newFileData(asset.path), ("Failed to collect file data for Atlas %s"):format(asset.name))
+	local image_data =
+		assert(love.image.newImageData(file_data), ("Failed to initialize image data for Atlas %s"):format(asset.name))
+	local image = love.graphics.newImage(image_data, { mipmaps = true, dpiscale = G.SETTINGS.GRAPHICS.texture_scaling })
+
+	G.ASSET_ATLAS[asset.name] = {
+		name = asset.name,
+		image = image,
+		type = asset.type,
+		px = asset.px,
+		py = asset.py,
+	}
+end
+function BUMod.setup_sprites()
+	-- Don't do anything if SMODS present
+	if SMODS and SMODS.can_load then
+		return
+	end
+
+	local assets_to_load = {
+		{
+			name = "Joker",
+			path = BUMod.PATH .. "/assets/" .. G.SETTINGS.GRAPHICS.texture_scaling .. "x/Jokers.png",
+			px = 71,
+			py = 95,
+		},
+		{
+			name = "Tarot",
+			path = BUMod.PATH .. "/assets/" .. G.SETTINGS.GRAPHICS.texture_scaling .. "x/Tarots.png",
+			px = 71,
+			py = 95,
+		},
+	}
+
+	for _, asset in ipairs(assets_to_load) do
+		BUMod.load_asset(asset)
+	end
+end
+function BUMod.setup_collabs()
+	if SMODS and SMODS.can_load then
+		return
+	end
+
+	for _, asset in ipairs(BUMod.COLLABS) do
+		BUMod.load_asset({
+			name = "bumod_" .. asset.key .. "_1",
+			path = BUMod.PATH
+				.. "/assets/"
+				.. G.SETTINGS.GRAPHICS.texture_scaling
+				.. "x/collabs/"
+				.. asset.key
+				.. "_1.png",
+			px = 71,
+			py = 95,
+		})
+		BUMod.load_asset({
+			name = "bumod_" .. asset.key .. "_2",
+			path = BUMod.PATH
+				.. "/assets/"
+				.. G.SETTINGS.GRAPHICS.texture_scaling
+				.. "x/collabs/"
+				.. asset.key
+				.. "_2.png",
+			px = 71,
+			py = 95,
+		})
+	end
+
+	if not BUMod.is_collabs_injected then
+		BUMod.is_collabs_injected = true
+		for _, collab in pairs(BUMod.COLLABS) do
+			local next_index = #G.COLLABS.options[collab.suit] + 1
+			G.COLLABS.options[collab.suit][next_index] = "bumod_" .. collab.key
+			BUMod.collabs_index_map[collab.suit]["bumod_" .. collab.key] = next_index
+		end
+		BUMod.setup_collabs_localization()
+	end
+end
+
+function BUMod.setup_collabs_localization()
+	if SMODS and SMODS.can_load then
+		return
+	end
+	if not G.localization then
+		return
+	end
+	for _, collab in ipairs(BUMod.COLLABS) do
+		local index = BUMod.collabs_index_map[collab.suit]["bumod_" .. collab.key]
+		if index then
+			G.localization.misc.collabs[collab.suit][tostring(index)] = collab.loc_txt
+		end
+	end
+end
--- steamodded.lua
+++ steamodded.lua
@@ -1,79 +1,6 @@
 if Malverk then
-	local textures_to_load = {
-		{
-			key = "joker_scholar_drspectred",
-			path = "single_jokers/j_scholar.png",
-			set = "Joker",
-			keys = { "j_scholar" },
-			loc_txt = {
-				name = "Scholar DrSpectred",
-				text = { "Replace Scholar with DrSpectred" },
-			},
-		},
-		{
-			key = "joker_showman_chip",
-			path = "single_jokers/j_ring_master.png",
-			set = "Joker",
-			keys = { "j_ring_master" },
-			loc_txt = {
-				name = "Showman Chip",
-				text = { "Replace Showman with Chip" },
-			},
-		},
-		{
-			key = "joker_space_zaino",
-			path = "single_jokers/j_space.png",
-			set = "Joker",
-			keys = { "j_space" },
-			loc_txt = {
-				name = "Spaceman Zaino",
-				text = { "Replace Spaceman with Zaino" },
-			},
-		},
-		{
-			key = "joker_duo_kitty_and_perkeo",
-			path = "single_jokers/j_duo.png",
-			set = "Joker",
-			keys = { "j_duo" },
-			loc_txt = {
-				name = "Kitty and Perkeo Duo",
-				text = { "Replace The Duo with Kitty and Perkeo" },
-			},
-		},
-		{
-			key = "joker_family_wingcap",
-			path = "single_jokers/j_family.png",
-			set = "Joker",
-			keys = { "j_family" },
-			loc_txt = {
-				name = "Wingcap's Family",
-				text = { "Replace The Family with Wingcap's Family" },
-			},
-		},
-		{
-			key = "joker_idol_stupid_cat",
-			path = "single_jokers/j_idol.png",
-			set = "Joker",
-			keys = { "j_idol" },
-			loc_txt = {
-				name = "Stupid's Cat Idol",
-				text = { "Replace The Idol with Stupid's Cat Idol" },
-			},
-		},
-		{
-			key = "tarot_hung_man",
-			path = "single_tarots/c_hanged_man.png",
-			set = "Tarot",
-			keys = { "c_hanged_man" },
-			loc_txt = {
-				name = "Hung Man",
-				text = { "Replace The Hanged Man with The Hung Man" },
-			},
-		},
-	}
-
 	local result_keys = {}
-	for _, definition in ipairs(textures_to_load) do
+	for _, definition in ipairs(BUMod.CARDS) do
 		table.insert(result_keys, "bumod_" .. definition.key)
 		AltTexture(definition)
 	end
@@ -111,5 +38,43 @@ else
 		path = "Tarots.png",
 		atlas_table = "ASSET_ATLAS",
 		raw_key = true,
+	})
+end
+
+for _, asset in ipairs(BUMod.COLLABS) do
+	local lc_atlas = SMODS.Atlas({
+		key = asset.key .. "_1",
+		path = "collabs/" .. asset.key .. "_1.png",
+		px = 71,
+		py = 95,
+		atlas_table = "ASSET_ATLAS",
+	})
+	local hc_atlas = SMODS.Atlas({
+		key = asset.key .. "_2",
+		px = 71,
+		py = 95,
+		path = "collabs/" .. asset.key .. "_2.png",
+		atlas_table = "ASSET_ATLAS",
+	})
+	SMODS.DeckSkin({
+		key = asset.key,
+		suit = asset.suit,
+		loc_txt = asset.loc_txt,
+		palettes = {
+			{
+				key = "lc",
+				ranks = { "Jack", "Queen", "King" },
+				display_ranks = { "Jack", "Queen", "King" },
+				pos_style = "collab",
+				atlas = lc_atlas.key,
+			},
+			{
+				key = "hc",
+				ranks = { "Jack", "Queen", "King" },
+				display_ranks = { "Jack", "Queen", "King" },
+				pos_style = "collab",
+				atlas = hc_atlas.key,
+			},
+		},
 	})
 end
